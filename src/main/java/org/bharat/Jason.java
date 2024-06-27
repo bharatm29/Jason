f088df0d46a493074871cb1918bc82fc85722489
Bharat Maheshwari <119489267+bharatm29@users.noreply.github.com>
2024-06-27 14:28:24 +0530

    minor refactors

--- src/main/java/org/bharat/Jason.java
+++ src/main/java/org/bharat/Jason.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
-public class Jason<T> {
+public class Jason {
     private StringBuilder json;
 
     private final boolean isRecord;
@@ -20,9 +20,17 @@ public class Jason<T> {
         this.json = new StringBuilder("{\n");
     }
 
-    public String serialize(Class<T> objClass, Object obj) {
+    /**
+     * <p>Serializes object into json
+     * </p>
+     * @param obj Object to serialize
+     * @return Serialized json
+     * @since 1.0
+     */
+    public String serialize(Object obj) {
         this.currObj = obj;
 
+        var objClass = obj.getClass();
         final var methods = objClass.getDeclaredMethods();
 
         for (final Field field : objClass.getDeclaredFields()) {
--- src/test/java/org/bharat/JasonTest.java
+++ src/test/java/org/bharat/JasonTest.java
@@ -9,14 +9,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("While testing Jason for json serialization, ")
 class JasonTest {
+    private Jason jason;
+
+    @BeforeEach
+    void setUp() {
+        this.jason = new Jason();
+    }
 
     @Test
     @DisplayName("Testing simple strings")
     void simpleStrings() {
-        Jason<JsonStr> jason = new Jason<>();
         JsonStr obj = new JsonStr("bharat", "maheshwari");
 
-        final var actualJson = jason.serialize(JsonStr.class, obj);
+        final var actualJson = jason.serialize(obj);
 
         final var expectedJson = """
                 {
@@ -31,10 +36,9 @@ class JasonTest {
     @Test
     @DisplayName("Testing strings, numbers and booleans")
     void strNumsBooleans() {
-        Jason<JsonStrNumBool> jason = new Jason<>();
         JsonStrNumBool obj = new JsonStrNumBool("bharat maheshwari", 19, 100.29, false);
 
-        final var actualJson = jason.serialize(JsonStrNumBool.class, obj);
+        final var actualJson = jason.serialize(obj);
 
         final var expectedJson = """
                 {
